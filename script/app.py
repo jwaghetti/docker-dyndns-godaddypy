624247b990a2239f5899fe52372d00e520bc86d0
Jean Waghetti <jean@waghetti.com>
2018-02-11 19:58:01 -0200

    Mail settings from environment variables.

--- script/app.py
+++ script/app.py
@@ -2,13 +2,31 @@
 
 #full package imports
 from datetime import datetime
+from email.mime.text import MIMEText
 import os
 import sys
+import smtplib
 import pif
 import time
 
 #partial imports
 from godaddypy import Client,Account
+
+def sendMail(ip, mailFrom, mailTo):
+  try:
+    msg = MIMEText('The IP address of asmodai host changed to: ' + ip)
+    msg['Subject'] = 'asmodai IP update: ' + ip
+    msg['From'] = mailFrom 
+    msg['To'] = mailTo 
+    smtp = smtplib.SMTP(smtpServer, smtpPort)
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.ehlo
+    smtp.login(smtpUser, smtpPassword)
+    smtp.sendmail(mailFrom, [mailTo], msg.as_string())
+    smtp.quit()
+  except:
+    print("Error sending update e-mail")
 
 domain = os.environ['DGDP_DOMAIN']
 #a_record='@'
@@ -18,9 +36,22 @@ api_secret = os.environ['DGDP_APISECRET']
 subdomains = os.environ['DGDP_SUBDOMAINS'].split(',')
 sleep_time = float(os.environ['DGDP_SLEEPTIME'])
 
+mailFrom = os.environ['DGDP_MAILFROM']
+mailTo = os.environ['DGDP_MAILTO']
+mailSubject = os.environ['DGDP_MAILSUBJECT']
+sendmail = bool(os.environ['DGDP_SENDMAIL'])
+smtpServer = os.environ['DGDP_SMTPSERVER']
+smtpPort = os.environ['DGDP_SMTPPORT']
+smtpUser = os.environ['DGDP_SMTPUSER']
+smtpPassword = os.environ['DGDP_SMTPPASSWORD']
+
+lastIP = '0.0.0.0'
+
 var = 1
 
 while var == 1:
+
+  try:
 
     userAccount = Account(api_key=api_key, api_secret=api_secret)
     userClient = Client(userAccount)
@@ -30,38 +61,41 @@ while var == 1:
     print(datetime.now())
     print(publicIP)
 
+    if (publicIP != lastIP and sendmail == True):
+      sendMail(publicIP, mailFrom, mailTo)
+
+
     # subdomains_tmp will contain the subdomains to be created
     # after the current registered subdomains are updated
     subdomains_tmp = list(subdomains)
     
-    try:
-        records = userClient.get_records(domain, record_type=record_type)
-        for record in records:
-            if record["name"] in subdomains:
-                print(record["name"])
-                subdomains_tmp.remove(record["name"])
-                if publicIP == record["data"]:
-                    updateResult = True
-                else:
-                    updateResult = userClient.update_record_ip(publicIP, domain, name=record["name"], record_type=record_type)
-                    print('Registry updated.')
+    records = userClient.get_records(domain, record_type=record_type)
+    for record in records:
+      if record["name"] in subdomains:
+        print(record["name"])
+        subdomains_tmp.remove(record["name"])
+        if publicIP == record["data"]:
+          updateResult = True
+        else:
+          updateResult = userClient.update_record_ip(publicIP, domain, name=record["name"], record_type=record_type)
+          print('Registry updated.')
 
-                if updateResult != True:
-                     print('Error updating registry.');
+        if updateResult != True:
+          print('Error updating registry.');
 
-        for subdomain in subdomains_tmp:
+    for subdomain in subdomains_tmp:
 
-            print(subdomain)
-            print('Creating subdomain');
+      print(subdomain)
+      print('Creating subdomain');
 
-            if userClient.add_record(domain, {'data':publicIP, 'name':subdomain, 'ttl':3600, 'type':record_type}):
-               print('Subdomain created')
-            else:
-               print('Subdomain creation error')
- 
+      if userClient.add_record(domain, {'data':publicIP, 'name':subdomain, 'ttl':3600, 'type':record_type}):
+        print('Subdomain created')
+      else:
+        print('Subdomain creation error')
 
+  except:
+      print(sys.exc_info()[1])
 
-    except:
-        print(sys.exc_info()[1])
+  lastIP = publicIP
 
-    time.sleep( sleep_time ) 
+  time.sleep( sleep_time ) 
